b948d71a60ccb14fad25b1ff79df293a5bde7798
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-12-06 14:05:08 +0100

    added test for normal fee return vaules for car and mc

--- src/TollCalculatorTest.java
+++ src/TollCalculatorTest.java
@@ -1,3 +1,27 @@
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TollCalculatorTest {
+    TollCalculator tollCalculator = new TollCalculator();
+    Car car = new Car();
+    Motorbike mc = new Motorbike();
+    LocalDateTime nonRushHour = LocalDateTime.of(2021, 12, 6, 10, 10, 19);
+    LocalDateTime morningRushHour = LocalDateTime.of(2021, 12, 6, 8, 10, 19);
+    LocalDateTime afternoonRushHour = LocalDateTime.of(2021, 12, 6, 16, 30, 0);
+    LocalDateTime[] weekend = {LocalDateTime.of(2021, 12, 5, 9, 15, 44)};
+
+
+    // Test av normaltaxa av bil samt MC
+    // Bilar kostar normalt 11 kr för att passera vägkamera
+    // Motorcyklar kostar normalt 8 kr för att passera vägkamera
+    @Test
+    public void testVechileFee() {
+
+        Assertions.assertEquals(11, car.getFee());
+        Assertions.assertEquals(8, mc.getFee());
+
+    }
+        
     
 }
