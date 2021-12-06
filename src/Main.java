cf372aff4c0d2f30b47b3c4b906ebc7f0aa46e25
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-12-06 15:34:51 +0100

    created Tollcalculator object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+        TollCalculator tollCalculator = new TollCalculator();
 
         Vehicle car = new Car("xtv123");
         Vehicle mc = new Motorbike("rqt394");
