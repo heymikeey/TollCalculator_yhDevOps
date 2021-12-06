11ecaf2ad11b0b25f6dd4cb36faf7e28bf46af60
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-12-06 14:59:15 +0100

    create 2 vechicle objects

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Vehicle car = new Car("xtv123");
+        Vehicle mc = new Motorbike("rqt394");
+
+
+    }
+}
