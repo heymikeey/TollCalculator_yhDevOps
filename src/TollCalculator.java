879b4e034df6f09d1d518f2fee288f81ebd187fa
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-12-06 15:20:11 +0100

    removed unused method and cleaned getTollFee method

--- src/TollCalculator.java
+++ src/TollCalculator.java
@@ -13,18 +13,14 @@ public class TollCalculator {
     }
 
     // Huvudmetod som räknar ut dagsavgift för en bil 
-    public int getTollFee(Vehicle vehicle, Date... dates) {
-        Date intervalStart = dates[0];
+    public int getTollFee(Vehicle vehicle, LocalDateTime[] dates) {
+
         int totalFee = 0;
+
         for (Date date : dates) {
             
         }
         return totalFee;
-    }
-    
-    // Räkna ut avgift för en bil en viss tid på dygnet
-    public int getTollFee(final Date date, Vehicle vehicle) {
-        return 0;
     }
     
     //Räkna ut avgift för en bil en viss tid på dygnet
