1d3406e0567bbbede74b336dc105ea29a6ecf427
Hannes Lindqvist <92028400+HannesLindqvist@users.noreply.github.com>
2021-12-07 18:26:33 +0100

    added daily fee method

--- src/TollCalculator.java
+++ src/TollCalculator.java
@@ -12,27 +12,33 @@ public class TollCalculator {
         this.maxTotal = 60;
     }
 
+
+    // Huvudmetod som räknar ut dagsavgift för en bil
+    public int getTotalTollFee(Vehicle vehicle, LocalDateTime[] dates) {
+
+        int totalFee = 0;
+
+        for (LocalDateTime date : dates) {
+            if (isTollFreeDate(date)) {
+                totalFee += weekendFee;
+            }
+            else if (isRushHour(date)){
+                totalFee += maxFee;
+            }
+            else totalFee += vehicle.getFee();
+        }
+        if (totalFee > maxTotal) totalFee = 60;
+
+        printTollFee(vehicle, dates, totalFee);
+        return totalFee;
+    }
+
+
     //  Räkna ut om det är helgdag
     public boolean isTollFreeDate(LocalDateTime date) {
         return (date.getDayOfWeek().toString().equals("SATURDAY") ||
                 date.getDayOfWeek().toString().equals("SUNDAY"));
     }
-
-
-    // Huvudmetod som räknar ut dagsavgift för en bil
-    /*public int getTollFee(Vehicle vehicle, LocalDateTime[] dates) {
-
-        int totalFee = 0;
-
-        for (Date date : dates) {
-            
-        }
-        return totalFee;
-    }
-
-     */
-    
-    // Räkna ut om det är helgdag
        
 
     // Räkna ut om det är rusningstid
