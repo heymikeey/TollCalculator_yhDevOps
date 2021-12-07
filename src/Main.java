bf785ff4460d54f7e9e46f3c074f4debfb2dba35
Mikael Jansson <92028583+heymikeey@users.noreply.github.com>
2021-12-07 19:59:39 +0100

    removed throw exception

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         TollCalculator tollCalculator = new TollCalculator();
 
         Vehicle car = new Car("xtv123");
         Vehicle mc = new Motorbike("rqt394");
         
         tollCalculator.getTotalTollFee(car, TimeStamps.getCamera3());
-        tollCalculator.getTotalTollFee(mc, TimeStamps. getCamera1());
-        tollCalculator.getTotalTollFee(car, TimeStamps. getCamera2());
+        tollCalculator.getTotalTollFee(mc, TimeStamps.getCamera1());
+        tollCalculator.getTotalTollFee(car, TimeStamps.getCamera2());
 
 
     }
